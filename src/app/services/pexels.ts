b99065cf6102a6bf2f16999432b031054ea75332
Bendada-abdelmajid <82829092+Bendada-abdelmajid@users.noreply.github.com>
2025-11-30 17:27:51 +0100

    add new features

--- src/app/services/pexels.ts
+++ src/app/services/pexels.ts
@@ -1,11 +1,11 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import 'dotenv/config';
+
 @Injectable({ providedIn: 'root' })
 export class PexelsService {
   private http = inject(HttpClient);
   private API_URL = 'https://api.pexels.com/v1/search';
-  private API_KEY = process.env['API_KEY'] ?? ""; 
+  private API_KEY = "w6wPgpdMz577w7H6IUJZHiTETXdTReGZFLYd6sTxTKRbrIaAcOYRKww0"; 
 
   getAvatars(query = 'cats', perPage = 12) {
     const headers = new HttpHeaders({ Authorization: this.API_KEY });
